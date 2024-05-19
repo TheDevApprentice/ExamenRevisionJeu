2cc7e7e955608ee1facb76cf4a554e1731264533
habric <80226527+TheDevApprentice@users.noreply.github.com>
2024-05-19 13:59:41 -0400

    Added particule system

--- 3D/Avion/Assets/Movement.cs
+++ 3D/Avion/Assets/Movement.cs
@@ -7,10 +7,12 @@ public class Movement : MonoBehaviour
     public float rotationSpeed = 10f;
     public float forwardForce = 50f;
     public GameObject smokeParticlesPrefab;
+    public Transform smokeParticlesParent; // Ajouter un transform pour le parent des particules
 
     private Rigidbody rb;
     private Transform helice;
     private bool isJumping = false;
+    private GameObject smokeParticlesInstance; // Ajouter une référence à l'instance des particules
 
     void Start()
     {
@@ -24,12 +26,20 @@ public class Movement : MonoBehaviour
         if (Input.GetButtonDown("Jump"))
         {
             isJumping = true;
+
+            if (smokeParticlesInstance == null)
+            {
+                smokeParticlesInstance = Instantiate(smokeParticlesPrefab, smokeParticlesParent); // Instancier les particules
+            }
+
+            smokeParticlesInstance.SetActive(true); // Activer l'émission de particules
         }
 
         // Lorsqu’on relâche `Jump`
         if (Input.GetButtonUp("Jump"))
         {
             isJumping = false;
+            smokeParticlesInstance.SetActive(false); // Désactiver l'émission de particules
         }
 
         // Si Jump est enfoncé
